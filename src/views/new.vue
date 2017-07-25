abaeb70d80502c9fac0b8359ed0af72027b573da
Your Name <you@example.com>
2017-07-25 15:26:34 -0700

    fix the bug

--- src/views/new.vue
+++ src/views/new.vue
@@ -81,18 +81,13 @@
                 var context = canvas.getContext('2d');
                 var img = new Image();
                 img.onload = () => {
-                    if (size > 900000) {
-                        canvas.width = img.width / 3;
-                        canvas.height = img.height / 3;
-                    } else if (size > 400000) {
-                        canvas.width = img.width / 2;
-                        canvas.height = img.height / 2;
-                    } else {
-                        canvas.width = img.width;
-                        canvas.height = img.height;
-                    }
                     canvas.width = img.width;
                     canvas.height = img.height;
+                    while (size > 200000) {
+                        canvas.width /= 1.5;
+                        canvas.height /= 1.5;
+                        size /= 2.25;
+                    }
                     context.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
                     tempImage = canvas.toDataURL('image/jpg');
                     this.smallimages.push(tempImage);
